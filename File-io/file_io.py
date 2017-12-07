8c650c1c8ce6ece95b508e1ed5a72747e2045b09
KennethA2001 <31477599+KennethA2001@users.noreply.github.com>
2017-12-07 09:22:41 -0500

    Add files via upload

---
+++ File-io/file_io.py
@@ -0,0 +1,29 @@
+import os
+import random
+
+path = "data"
+
+
+file_names = os.listdir(path)
+
+for i, f in enumerate(file_names):
+    print(str(i) + ") " + f)
+
+choice = input("pick one ")
+choice = int(choice)
+
+file = "data/" + file_names[choice]
+
+
+with open(file, 'r') as f:
+    lines = f.read().splitlines()
+
+print(lines)
+
+category_name = [0]
+puzzle = random.choice( lines[1:] )
+
+print(category_name)
+print(puzzle)
+
+    
